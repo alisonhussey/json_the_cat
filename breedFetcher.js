517112d5dca7bec153d859e109cb374d0d531800
alisonhussey <alisonhussey@gmail.com>
2020-12-03 22:10:56 +0000

    a command line app to query dataset from the terminal,updated

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,17 @@
 const request = require('request');
 const arguments = process.argv.slice(2);
 const breed = arguments[0];
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
 
-request(`https://api.thecapi.com/v1/breeds/search?q=${breed}`, (err, response, body) => {
+request(url, (err, response, body) => {
   if (err) {
     console.log('url invalid', err);
   } else {
     const data = JSON.parse(body);
-    console.log(data[0].description);
     if (data.length === 0) {
       console.log('breed does not exist');
+    } else {
+      console.log(data[0].description);
     }
   }
 });
-
